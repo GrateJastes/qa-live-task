193279e22116551982b062759b8bff20905fd349
Prince George <prince@Jomols-MacBook-Air.local>
2025-10-02 14:16:10 +0200

    wrote cypress test script

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,55 +1,83 @@
 describe('Tasks app - messy tests', () => {
-  it('validation shows error if title missing', () => {
+  beforeEach(() => {
     cy.visit('/');
-    cy.get('button').contains('Add Task').click();
+    cy.get('#taskForm').should('exist');
+  });
+  it('validation shows error if title missing', () => {
+    cy.get('#save').click();
     cy.get('#titleError').should('be.visible');
   });
 
   it('add task and check priority badge', () => {
-    cy.visit('/');
-    cy.get('input').first().type('Write tests');
-    cy.get('textarea').type('some desc');
+    cy.get('#title').first().type('Write tests');
+    cy.get('#description').type('some desc');
     cy.get('input[type="date"]').type('2025-10-01');
-    cy.get('select').select('high');
-    cy.get('button').contains('Add Task').click();
-
+    cy.get('#priority').select('high');
+    cy.get('#save').click();
     cy.wait(300);
 
-    cy.contains('Write tests');
-    cy.get('.badge.high').should('exist');
+    cy.contains('#taskList li','Write tests').should('exist');
+    cy.contains('#taskList li', 'Write tests').find('.badge.high').should('exist');
+
+
   });
 
   it('complete and filter', () => {
-    cy.visit('/');
     cy.get('#title').type('A task');
     cy.get('#save').click();
-    cy.wait(200);
     cy.get('#title').type('B task');
     cy.get('#save').click();
-    cy.wait(200);
 
-    cy.get('input[type="checkbox"]').first().check();
-    cy.get('select').eq(1).select('completed');
-    cy.contains('A task');
+    cy.contains('#taskList li', 'A task')
+      .find('input[type="checkbox"]')
+      .check();
+
+    cy.get('#filterStatus').select('completed');
+    cy.contains('#taskList li', 'A task').should('exist');
+    cy.contains('#taskList li', 'B task').should('not.exist');
   });
 
   it('edit flow', () => {
-    cy.visit('/');
     cy.get('#title').type('Original');
     cy.get('#save').click();
-    cy.wait(200);
-    cy.contains('Edit').click();
+
+    cy.contains('#taskList li', 'Original')
+      .find('button')
+      .contains('Edit')
+      .click();
+
     cy.get('#title').clear().type('Edited');
     cy.get('#save').click();
-    cy.contains('Edited');
+
+    cy.contains('#taskList li', 'Edited').should('exist');
   });
 
   it('delete task', () => {
-    cy.visit('/');
     cy.get('#title').type('Will be deleted');
     cy.get('#save').click();
-    cy.wait(200);
-    cy.contains('Delete').click();
-    cy.contains('Will be deleted').should('not.exist');
+
+    cy.contains('#taskList li', 'Will be deleted')
+      .find('button')
+      .contains('Delete')
+      .click();
+
+    cy.contains('#taskList li', 'Will be deleted').should('not.exist');
+  });
+
+  it('checks resetForm functionality', () => {
+    // Fill the form fields
+    cy.get('#title').type('Test reset');
+    cy.get('#description').type('Reset description');
+    cy.get('input[type="date"]').type('2025-10-01');
+    cy.get('#priority').select('medium');
+
+    // Click the reset button
+    cy.get('#reset').click();
+
+    // Verify that fields are cleared
+    cy.get('#title').should('have.value', '');
+    cy.get('#description').should('have.value', '');
+    cy.get('input[type="date"]').should('have.value', '');
+    cy.get('#priority').should('have.value', 'medium'); // default selected
   });
 });
